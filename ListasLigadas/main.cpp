25a3b3e989487a72924bd5ccbd10a58eb54ebbc8
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-22 11:13:56 -0500

    Primer commit

---
+++ ListasLigadas/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+//#include <conio.h>
+
+using namespace std;
+
+struct Nodo
+{
+    int dato;
+    Nodo *siguiente;
+};
+
+//Prototipos de funciones
+void insertarLista(Nodo *&, int);
+void mostrarLista(Nodo *);
+
+int main() {    
+    Nodo *lista = NULL;
+    int dato;
+
+    cout<<"Ingrese un número: \n";
+    cin>>dato;
+    insertarLista(lista,dato);
+
+    cout<<"Ingrese un número: \n";
+    cin>>dato;
+    insertarLista(lista,dato);
+
+    cout<<"Ingrese un número: \n";
+    cin>>dato;
+    
+    insertarLista(lista,dato);
+    mostrarLista(lista);
+
+    return 0;
+}
+
+void insertarLista(Nodo *&lista, int n) {
+    Nodo *nuevo_nodo = new Nodo();
+    nuevo_nodo -> dato = n;
+
+    Nodo *aux1 = lista;
+    Nodo *aux2;
+
+    while ((aux1 != NULL) && (aux1->dato != n)) {
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+    
+    if(lista == aux1) {
+        lista = nuevo_nodo;
+    } else {
+        aux2->siguiente = nuevo_nodo;
+    }
+    nuevo_nodo->siguiente = aux1;
+    cout<<"Elemento "<< n << " insertado correctamente\n";
+    
+}
+
+void mostrarLista(Nodo *lista) {
+    Nodo *actual = new Nodo();
+    actual = lista;
+
+    while (actual != NULL) {
+        cout<< actual->dato<<" -> ";
+        actual = actual->siguiente;
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+**Compilar el programa**
+``` bash
+g++ nombre_programa.cpp
+```
+**Ejecutar el programa**
+``` bash
+./a.out
+```
